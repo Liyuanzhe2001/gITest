a2ea184cf5f4cb22e96702b20b4c01651522f3fc
Liyuanzhe2001 <619356147@qq.com>
2024-09-19 22:08:21 +0800

    finish delete & put

--- cmd/delete.go
+++ cmd/delete.go
@@ -5,6 +5,11 @@ package cmd
 
 import (
 	"fmt"
+	tmpl "gITest/template"
+	"gITest/utils"
+	"io/ioutil"
+	"strings"
+	"text/template"
 
 	"github.com/spf13/cobra"
 )
@@ -14,20 +19,60 @@ var deleteCmd = &cobra.Command{
 	Use:   "delete",
 	Short: "Send DELETE request to URL",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("delete called")
+		// 获取参数
+		url, err := cmd.Flags().GetString("url")
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		output, err := cmd.Flags().GetString("output")
+		if err != nil {
+			fmt.Println(err)
+		}
+		headers, err := cmd.Flags().GetStringSlice("headers")
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		if (len(headers) != 0 || output != "") && url == "" {
+			fmt.Println("Unable to find URL")
+			return
+		}
+		if url == "" {
+			cmd.HelpFunc()(cmd, []string{})
+			return
+		}
+		// 参数检查没问题 发送GET请求
+		out, err := utils.Net.DELETE(url, headers)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Println(out.Response)
+		// 需要保存结果
+		if output != "" {
+			parse, _ := template.New("out").Parse(tmpl.GetTemplate)
+			var b strings.Builder
+			if err := parse.Execute(&b, out); err != nil {
+				fmt.Println("Error executing template:", err)
+				return
+			}
+			err = ioutil.WriteFile(output, []byte(b.String()), 0644)
+			if err != nil {
+				fmt.Println("Error writing file:", err)
+				return
+			}
+		}
 	},
 }
 
 func init() {
+	deleteCmd.Flags().StringP("url", "u", "", "url")
+	deleteCmd.Flags().StringSliceP("headers", "h", []string{}, "请求头，K1:V1,K2:V2...的格式")
+	deleteCmd.Flags().StringP("output", "o", "", "本次测试保存位置")
+	// deletet方法的参数在url中
+	//deleteCmd.Flags().StringP("param", "p", "{}", "请求参数")
+	deleteCmd.Flags().Bool("help", false, "显示帮助信息")
+	deleteCmd.Flags().MarkHidden("help")
 	rootCmd.AddCommand(deleteCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// deleteCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// deleteCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
--- utils/net.go
+++ utils/net.go
@@ -32,7 +32,7 @@ func parseHeaders(req *http.Request, headers []string) (string, error) {
 		split := strings.Split(s, ":")
 		req.Header.Set(split[0], split[1])
 	}
-	if req.Method == "POST" {
+	if req.Method == "POST" || req.Method == "PUT" {
 		req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 	} else {
 		req.Header.Set("Content-Type", "application/json")
@@ -130,6 +130,7 @@ func (n *net) GET(url string, headers []string) (*data.GetData, error) {
 	return getData, nil
 }
 
+// POST 请求
 func (n *net) POST(url string, headers []string, params []string) (*data.PostData, error) {
 	postData := &data.PostData{}
 
@@ -140,6 +141,123 @@ func (n *net) POST(url string, headers []string, params []string) (*data.PostDat
 	client := &http.Client{}
 	// 创建请求
 	req, err := http.NewRequest("POST", url, nil)
+	if err != nil {
+		return postData, err
+	}
+	postData.Url = url
+
+	// 解析请求头
+	headersJson, err := parseHeaders(req, headers)
+	if err != nil {
+		return postData, err
+	}
+	postData.Headers = headersJson
+
+	// 解析请求参数
+	paramsJson, err := parseParams(req, params)
+	if err != nil {
+		return postData, err
+	}
+	postData.Params = paramsJson
+
+	// 发送请求
+	resp, err := client.Do(req)
+	if err != nil {
+		return postData, err
+	}
+	defer resp.Body.Close()
+
+	// 获取内容
+	all, err := io.ReadAll(resp.Body)
+	// 获取结果失败
+	if err != nil {
+		return postData, err
+	}
+
+	// 用于保存json结果
+	var data map[string]string
+	err = json.Unmarshal(all, &data)
+	// 如果err不为空 说明返回的结果不能被解析成json
+	if err != nil {
+		postData.Response = string(all)
+		return postData, nil
+	}
+	// 使用json.MarshalIndent格式化输出
+	responseJson, err := json.MarshalIndent(data, "", "  ")
+	if err != nil {
+		return postData, err
+	}
+	postData.Response = string(responseJson)
+
+	return postData, nil
+}
+
+// DELETE 请求
+func (n *net) DELETE(url string, headers []string) (*data.GetData, error) {
+	getData := &data.GetData{}
+
+	// 检查url
+	url = addSchemeToURL(url)
+
+	// 创建客户端
+	client := &http.Client{}
+	// 创建请求
+	req, err := http.NewRequest("DELETE", url, nil)
+	if err != nil {
+		return getData, err
+	}
+	getData.Url = url
+
+	// 解析请求头
+	headersJson, err := parseHeaders(req, headers)
+	if err != nil {
+		return getData, err
+	}
+	getData.Headers = headersJson
+
+	// 发送请求
+	resp, err := client.Do(req)
+	if err != nil {
+		return getData, err
+	}
+	defer resp.Body.Close()
+
+	// 获取内容
+	all, err := io.ReadAll(resp.Body)
+	// 获取结果失败
+	if err != nil {
+		return getData, err
+	}
+
+	// 用于保存json结果
+	var data map[string]string
+	err = json.Unmarshal(all, &data)
+	// 如果err不为空 说明返回的结果不能被解析成json
+	if err != nil {
+		getData.Response = string(all)
+		return getData, nil
+	}
+	// 使用json.MarshalIndent格式化输出
+	responseJson, err := json.MarshalIndent(data, "", "  ")
+	if err != nil {
+		return getData, err
+	}
+	getData.Response = string(responseJson)
+
+	return getData, nil
+}
+
+// PUT 请求
+func (n *net) PUT(url string, headers []string, params []string) (*data.PostData, error) {
+	postData := &data.PostData{}
+
+	// 检查url
+	url = addSchemeToURL(url)
+
+	// 创建客户端
+	client := &http.Client{}
+	// 创建请求
+	req, err := http.NewRequest("PUT", url, nil)
 	if err != nil {
 		return postData, err
 	}
